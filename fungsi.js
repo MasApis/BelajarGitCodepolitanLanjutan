6feb25d1f50fe44dd86a64830ea650b66199c994
ABDUL HAFIDZ <abdulhafidz3105@gmail.com>
2025-01-09 21:13:22 +0700

    Menambahkan fitur biodata author

--- fungsi.js
+++ fungsi.js
@@ -7,3 +7,13 @@ console.log("Hasil dari 250 + 200 adalah " + penjumlahan(250, 200));
 console.log("Hasil dari 250 - 200 adalah " + pengurangan(250, 200));
 console.log("Hasil dari 250 * 200 adalah " + perkalian(250, 200));
 console.log("Hasil dari 480 / 20  adalah " + pembagian(480, 20));
+
+// biodata abdul hafidz
+console.log("Nama : ABDUL HAFIDZ");
+console.log("Tanggal lahir : Kijang, 31 Mei 2005");
+console.log("Umur : 20 tahun");
+console.log("Pekerjaan : Mahasiswa");
+console.log("Status perkawinan : belum kawin");
+console.log("Nomor handphone : 083146949357");
+console.log("Email : abdulhafidz3105@gmail.com");
+console.log("Alamat : Kp. Sidomulyo km 18 kijang");
