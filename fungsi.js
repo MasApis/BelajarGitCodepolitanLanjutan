4456fd90af46610c5c3cf22b673dd373139ebb57
ABDUL HAFIDZ <abdulhafidz3105@gmail.com>
2025-01-09 21:05:30 +0700

    Membuat fungsi aritmatika pada file js

---
+++ fungsi.js
@@ -0,0 +1,9 @@
+// perkalian
+const penjumlahan = (angka1, angka2) => angka1 + angka2;
+const pengurangan = (angka1, angka2) => angka1 - angka2;
+const perkalian = (angka1, angka2) => angka1 * angka2;
+const pembagian = (angka1, angka2) => angka1 / angka2;
+console.log("Hasil dari 250 + 200 adalah " + penjumlahan(250, 200));
+console.log("Hasil dari 250 - 200 adalah " + pengurangan(250, 200));
+console.log("Hasil dari 250 * 200 adalah " + perkalian(250, 200));
+console.log("Hasil dari 480 / 20  adalah " + pembagian(480, 20));
